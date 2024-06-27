b00c659f7572a7b40b46a7e711a6ae793371b09c
Nicovt0 <nicovt2005@gmail.com>
2024-06-27 05:13:12 -0400

    Se implementa el login

--- Proyecto-ToyTraders/app/forms.py
+++ Proyecto-ToyTraders/app/forms.py
@@ -5,3 +5,14 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = '__all__'
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Password'}))
+    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'check-input'}))
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Password'}))
+    terms_conditions = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'check-input'}))
