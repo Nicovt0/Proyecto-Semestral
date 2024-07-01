b575710dd9acbb5edfac78544633bc249e1315ed
Nicovt0 <nicovt2005@gmail.com>
2024-06-30 22:53:34 -0400

    Se modificaron los registros e inicios de sesion del proyecto

--- Proyecto-ToyTraders/app/forms.py
+++ Proyecto-ToyTraders/app/forms.py
@@ -11,13 +11,6 @@ class ProductoForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Password'}))
-    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'check-input'}))
-
-class SignUpForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Username'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Email'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Password'}))
-    terms_conditions = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'check-input'}))
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
--- Proyecto-ToyTraders/app/static/app/JS/preloader.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    alert('he cargado completamente la pagina')
-    $('#onload').fadeOut();
-    $('body').removeClass('fondo');
-}
